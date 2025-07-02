e3fad23dbee7024a91f4607901de9ccb2ec9c61e
xSkyLite <chkalovua11@gmail.com>
2025-07-02 14:51:35 +0300

    feat(weather): add getWeatherUseCase

---
+++ src/core/use-cases/get-weather.use-case.ts
@@ -0,0 +1,26 @@
+import { WeatherRepository } from "../../infrastructure/repositories/weather.repository";
+import { WeatherService } from "../../infrastructure/services/weather.service";
+import { WeatherEntity } from "../entities/weather.entity";
+
+export class getWeatherUseCase {
+  private repository: WeatherRepository;
+  private service: WeatherService;
+
+  constructor(repository: WeatherRepository, service: WeatherService) {
+    this.repository = repository;
+    this.service = service;
+  }
+
+  async execute(location: string, lang: string): Promise<WeatherEntity> {
+    const cached = await this.repository.getWeatherByLocation(location)
+
+    if (cached && cached.expiresAt > new Date()) {
+      return cached;
+    }
+
+    const fresh = await this.service.getCurrentWeather(location, lang);
+    await this.repository.upsertWeather(fresh)
+
+    return fresh;
+  }
+}
